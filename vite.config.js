6dec968c6c4fbeaa5d60c149d5459a88b83479ab
ALMacbeth <92785729+ALMacbeth@users.noreply.github.com>
2026-07-28 16:29:13 +0100

    set root asset location for webhosting

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import plugin from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: '/SoAInterface/',
     plugins: [plugin()],
     server: {
         port: 49396,
